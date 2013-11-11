890ae51e7d8534c4525070be5550d75967a824e8
Joseph Near <jnear@csail.mit.edu>
2013-11-11 15:36:36 -0500

    removed remove things

--- lib/railgrinder/railgrinder.rb
+++ lib/railgrinder/railgrinder.rb
@@ -824,7 +824,7 @@ class Analyzer
 
     results = Hash.new
     controller_klasses = ActionController::Base.descendants
-    controller_klasses = [NotesController] # remove
+    #controller_klasses = [NotesController] # remove
     log "here are the controllers and their actions that I know of"
 
     controller_klasses.each do |c|
@@ -836,7 +836,7 @@ class Analyzer
     controller_klasses.each do |controller|
       controller.action_methods.each do |action|
 
-        next unless action.to_s == "show" # remove
+        #next unless action.to_s == "show" # remove
 
         puts "EEE " + action.to_s
         begin
